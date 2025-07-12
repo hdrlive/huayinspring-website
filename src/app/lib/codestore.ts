806559a03fc24ca0684491cd95cff0d84e663fe8
hdrlive <hdrlivecn@outlook.com>
2025-07-12 22:35:39 +0200

    Create codestore.ts

---
+++ src/app/lib/codestore.ts
@@ -0,0 +1,9 @@
+const codes = new Map<string, string>();
+
+export function saveCode(email: string, code: string) {
+  codes.set(email, code);
+}
+
+export function getCode(email: string): string | undefined {
+  return codes.get(email);
+}
