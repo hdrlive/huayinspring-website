7b9717fe5c0ec4700bb9452f62dce13d014b7037
hdrlive <hdrlivecn@outlook.com>
2025-07-12 22:38:39 +0200

    Update codestore.ts

--- src/app/lib/codestore.ts
+++ src/app/lib/codestore.ts
@@ -1,9 +1,15 @@
-const codes = new Map<string, string>();
+// src/app/lib/codeStore.ts
+
+const codeStore = new Map<string, string>();
 
 export function saveCode(email: string, code: string) {
-  codes.set(email, code);
+  codeStore.set(email, code);
 }
 
 export function getCode(email: string): string | undefined {
-  return codes.get(email);
+  return codeStore.get(email);
+}
+
+export function deleteCode(email: string) {
+  codeStore.delete(email);
 }
