f0891ed32b6df9a0a6b07611693288b8394c22dc
hdrlive <hdrlivecn@outlook.com>
2025-07-12 22:36:51 +0200

    Delete src/app/lip directory

--- src/app/lip/codestore.ts
+++
@@ -1,9 +0,0 @@
-const codes = new Map<string, string>();
-
-export function saveCode(email: string, code: string) {
-  codes.set(email, code);
-}
-
-export function getCode(email: string): string | undefined {
-  return codes.get(email);
-}
