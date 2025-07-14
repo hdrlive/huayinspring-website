c1aa016577bde542af2584bc3f85df0aaea4c7f0
hdrlive <hdrlive@outlook.com>
2025-07-14 10:06:18 +0200

    Add next.config.js in root + update tsconfig alias

---
+++ next.config.js
@@ -0,0 +1,14 @@
+// next.config.js
+const path = require('path');
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  webpack(config) {
+    // Alias „@“ → <project_root>/src
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
+  },
+};
+
+module.exports = nextConfig;
